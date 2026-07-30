d12adf24df8c754d3cc0fae6c8214cf66c167e9b
tgoussot <narrra@protonmail.com>
2026-07-30 17:16:20 +0200

    🔧 Branche les routeurs de jeu sur l'application Express

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,11 @@ import "reflect-metadata";
 import express, {json} from "express";
 import { AppDataSource } from "./data-source";
 import {UtilisateursRouteur} from "./routes/UtilisateursRoute";
+import {SessionsRouteur} from "./routes/SessionsRoute";
+import {ParticipantsRouteur} from "./routes/ParticipantsRoute";
+import {ThemesRouteur} from "./routes/ThemesRoute";
+import {QuestionsRouteur} from "./routes/QuestionsRoute";
+import {CartesRouteur} from "./routes/CartesRoute";
 import cookieParser = require("cookie-parser");
 
 
@@ -19,6 +24,11 @@ async function main() {
         res.send("API fonctionnel");
     });
     app.use("/api/utilisateurs", UtilisateursRouteur);
+    app.use("/api/sessions", SessionsRouteur);
+    app.use("/api/participants", ParticipantsRouteur);
+    app.use("/api/themes", ThemesRouteur);
+    app.use("/api/questions", QuestionsRouteur);
+    app.use("/api/cartes", CartesRouteur);
 
     app.use((req, res) => {
         res.status(404).json({ message: "not found" });
